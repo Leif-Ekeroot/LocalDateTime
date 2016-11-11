b12f4a647ef06ba2d5c7ea41d033e36acc159909
User <User@7GZ7J32>
2016-11-11 16:15:34 +0100

    first commit

--- src/LocalDateTimeMain.java
+++ src/LocalDateTimeMain.java
@@ -6,7 +6,6 @@ public class LocalDateTimeMain {
     public static void main(String[] args) {
         LocalDateTime now = LocalDateTime.now();
               
-        // Testing
         System.out.println(now);
     }
 
